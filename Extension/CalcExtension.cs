47d3cc72932c7809aa43b293745f6aa6a6a1a46c
TejShinde <shindetejal995@gmail.com>
2024-11-19 23:53:52 +0530

    Add project files.

---
+++ Extension/CalcExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class CalcExtension
+    {
+        // extension method syntax
+        // first parameter should be the name of class, for which we want to add method
+        public static int Muliply(this Calculator c1, int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+}
---
+++ Extension/ExtensionMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class ExtensionMain
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            Console.WriteLine(c1.Muliply(2, 4));
+            Console.WriteLine(c1.Add(44, 66));
+        }
+    }
+}
